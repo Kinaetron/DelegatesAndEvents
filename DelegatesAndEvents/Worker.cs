20c2de6d7636b4508e9a6fb2fb6fb0074114f3f7
Darren <darren.daley@hotmail.co.uk>
2020-01-14 10:37:31 +0000

    some actual work

---
+++ DelegatesAndEvents/Worker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public class Worker
+    {
+        public event WorkPerformedHandler WorkPerformed;
+        public event EventHandler WorkComplete;
+
+        public void DoWork(int hours, WorkType workType)
+        {
+        }
+
+    }
+}
