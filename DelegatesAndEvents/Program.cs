052cb5d1a7bc6b2c58ee92dccd56f299676bc551
Darren <darren.daley@hotmail.co.uk>
2020-01-21 10:29:40 +0000

    more work done

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -1,16 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
+    public delegate int BizRulesDelegate(int x, int y);
+
     class Program
     {
         static void Main(string[] args)
         {
+            var data = new ProcessData();
+
+            BizRulesDelegate addDel = (x, y) => x + y;
+            BizRulesDelegate multiplyDel = (x, y) => x * y;
+            data.Process(2, 3, multiplyDel);
+
+            Func<int, int, int> funcAddDel = (x, y) => x + y;
+            Func<int, int, int> funcMultipleDel = (x, y) => x * y;
+            data.ProcessFunc(2, 3, funcAddDel);
+
+            Action<int, int> myAction = (x, y) => Console.WriteLine(x + y);
+            Action<int, int> myMultiplyAction = (x, y) => Console.WriteLine(x * y);
+            data.ProcessAction(2, 3, myMultiplyAction);
+                 
             var worker = new Worker();
+            worker.WorkPerformed += (s, e) => Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+            worker.WorkComplete += (s, e) => Console.WriteLine("Worker is completed");
+
+            worker.DoWork(8, WorkType.GenerateReports);
 
             Console.Read();
         }
